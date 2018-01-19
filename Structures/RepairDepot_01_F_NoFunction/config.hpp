cbd687765f1c9b7851a90a47ea1fc398e3c166cd
jameshruby <windstormer4@gmail.com>
2018-01-19 16:04:21 +0100

    RepairDepot variants added

--- Structures/RepairDepot_01_F_NoFunction/config.hpp
+++ Structures/RepairDepot_01_F_NoFunction/config.hpp
@@ -1,6 +1,21 @@
 class Land_RepairDepot_01_civ_F;
+class Land_RepairDepot_01_tan_F;
+class Land_RepairDepot_01_green_F;
+
 class Land_RepairDepot_01_civ_F_NoFunction: Land_RepairDepot_01_civ_F
 {	
-    displayName = $STR_TM_RepairDepotDisplayName;
+    displayName = $STR_TM_RepairDepotCivilianDisplayName;
+    transportRepair = 0;
+};
+
+class Land_RepairDepot_01_green_F_NoFunction: Land_RepairDepot_01_green_F
+{	
+    displayName = $STR_TM_RepairDepotGreenDisplayName;
+    transportRepair = 0;
+};
+
+class Land_RepairDepot_01_tan_F_NoFunction: Land_RepairDepot_01_tan_F
+{	
+    displayName = $STR_TM_RepairDepotTanDisplayName;
     transportRepair = 0;
 };
