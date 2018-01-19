088ec87e1ccc4d5e24f5bcd92f11b8a525150eca
jameshruby <windstormer4@gmail.com>
2018-01-19 18:32:44 +0100

    missing attribute loaders added

--- CustomAttributes/attributes.hpp
+++ CustomAttributes/attributes.hpp
@@ -1,5 +1,15 @@
 class TankResources_maxHoldActonDuration: Slider
-{
+{	
+	onLoad = "\
+		comment 'DO NOT COPY THIS CODE TO YOUR ATTRIBUTE CONFIG UNLESS YOU ARE CHANGING SOMETHING IN THE CODE!';\
+		_ctrlGroup = _this select 0;\
+		[_ctrlGroup controlsgroupctrl 100,_ctrlGroup controlsgroupctrl 101,''] call bis_fnc_initSliderValue;\
+	";
+	attributeLoad = "\
+		comment 'DO NOT COPY THIS CODE TO YOUR ATTRIBUTE CONFIG UNLESS YOU ARE CHANGING SOMETHING IN THE CODE!';\
+		_ctrlGroup = _this;\
+		[_ctrlGroup controlsgroupctrl 100,_ctrlGroup controlsgroupctrl 101,'',_value] call bis_fnc_initSliderValue;\
+	";
 	class Controls: Controls
 	{
 		class Title: Title{};
