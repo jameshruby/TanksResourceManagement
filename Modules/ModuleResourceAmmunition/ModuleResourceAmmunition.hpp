e5f22ab4ec13226b162038257c019a024ded4d0d
jameshruby <windstormer4@gmail.com>
2018-01-22 17:26:02 +0100

    white spacing

--- Modules/ModuleResourceAmmunition/ModuleResourceAmmunition.hpp
+++ Modules/ModuleResourceAmmunition/ModuleResourceAmmunition.hpp
@@ -1,38 +1,38 @@
-	class ModuleResourceAmmunition : ResourceModulesArea
+class ModuleResourceAmmunition : ResourceModulesArea
+{
+	icon = "A3\Ui_f\data\IGUI\Cfg\simpleTasks\types\rearm_ca.paa"; 
+	scope = 2;
+	displayName = $STR_TM_ResourceAmmunition;
+	
+	class ResourcesModuleParams : ResourcesModuleParams
 	{
-		icon = "A3\Ui_f\data\IGUI\Cfg\simpleTasks\types\rearm_ca.paa"; 
-		scope = 2;
-		displayName = $STR_TM_ResourceAmmunition;
-		
-		class ResourcesModuleParams : ResourcesModuleParams
+		title = $STR_TM_HoldAction_rearm;
+		progressIcon = "A3\TanksResourceManagement\Img\holdAction_rearm_CA.paa";
+		idleIcon =  "A3\TanksResourceManagement\Img\holdAction_rearm_CA.paa";
+		cfgVehiclesQuery = "configName _x isKindOf('B_Slingload_01_Ammo_F') && getNumber (_x >> 'transportAmmo') == 0";
+	};
+
+	class Attributes: Attributes
+	{
+		class MaxDuration : MaxDuration{};
+		class CreateBuilding : CreateBuilding{};
+		class BuildingType: BuildingType
 		{
-			title = $STR_TM_HoldAction_rearm;
-			progressIcon = "A3\TanksResourceManagement\Img\holdAction_rearm_CA.paa";
-			idleIcon =  "A3\TanksResourceManagement\Img\holdAction_rearm_CA.paa";
-			cfgVehiclesQuery = "configName _x isKindOf('B_Slingload_01_Ammo_F') && getNumber (_x >> 'transportAmmo') == 0";
+			defaultValue = """B_Slingload_01_Ammo_F_NoFunction""";
 		};
 
-		class Attributes: Attributes
+		class DrawBuildingMarkerIcon: DrawBuildingMarkerIcon{};
+
+		class BuildingMarker : BuildingMarker
 		{
-			class MaxDuration : MaxDuration{};
-			class CreateBuilding : CreateBuilding{};
-			class BuildingType: BuildingType
-			{
-				defaultValue = """B_Slingload_01_Ammo_F_NoFunction""";
-			};
-
-			class DrawBuildingMarkerIcon: DrawBuildingMarkerIcon{};
-
-			class BuildingMarker : BuildingMarker
-			{
-				defaultValue = """loc_RearmStation""";
-			};
-
-			class ModuleDescription: ModuleDescription{};			
+			defaultValue = """loc_RearmStation""";
 		};
 
-		class ModuleDescription: ModuleDescription
-		{
-			description = $STR_TM_ResourceAmmunitionDescription;
-		};
-	}; 
+		class ModuleDescription: ModuleDescription{};			
+	};
+
+	class ModuleDescription: ModuleDescription
+	{
+		description = $STR_TM_ResourceAmmunitionDescription;
+	};
+}; 
